074e91ec7a05febccdb3fc23e94ea9b5af66383e
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2025-12-19 00:00:32 +0530

    Add Pinecone index creation logic and refactor vector store initialization

--- backend/app/services/vector_store/pinecone_vector_store.py
+++ backend/app/services/vector_store/pinecone_vector_store.py
@@ -1,10 +1,28 @@
+import time
 from langchain_pinecone import PineconeVectorStore
-from pinecone import Pinecone
+from pinecone import Pinecone, ServerlessSpec
 
 from app.utils.envs import Envs
 from app.services.embedder.factory import get_embedder
 
 pc = Pinecone(api_key=Envs.PINECONE_API_KEY)
-index = pc.Index(Envs.PINECONE_INDEX_NAME)
+index_name = Envs.PINECONE_INDEX_NAME
 
-pinecone_vector_store = PineconeVectorStore(embedding=get_embedder(), index=index)
+if not pc.has_index(index_name):
+    print("Creating Pinecone index...")
+    pc.create_index(
+        name=index_name,
+        dimension=get_embedder()["dimension"],
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1")
+    )
+    while not pc.describe_index(index_name).status["ready"]:
+        time.sleep(1)
+    print("Created Pinecone index.")
+
+index = pc.Index(index_name)
+
+pinecone_vector_store = PineconeVectorStore(
+    embedding=get_embedder()["embedder"], 
+    index=index
+)
