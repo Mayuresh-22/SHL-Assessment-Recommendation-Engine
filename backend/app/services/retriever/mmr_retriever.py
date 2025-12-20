92202872c459b5d5f49c954a381bfa3808aa66c2
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2025-12-20 20:52:06 +0530

    Refactor: make lambda_mult arg configurable via env vars

--- backend/app/services/retriever/mmr_retriever.py
+++ backend/app/services/retriever/mmr_retriever.py
@@ -7,6 +7,6 @@ mmr_retriever = get_vector_store().as_retriever(
     search_kwargs={
         "k": Envs.TOP_K,
         "fetch_k": Envs.FETCH_K,
-        "lambda_mult": 1
+        "lambda_mult": Envs.LAMBDA_MULT
     }
 )
